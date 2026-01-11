8ae186e3e35cf4aa38e4bd8aa0427cd55dbeed01
Oxigen-tz <bertreux.charles@gmail.com>
2026-01-11 02:08:23 +0100

    fix : bug whritescreen

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,6 +3,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   // 🟢 UTILISATION DES VARIABLES D'ENVIRONNEMENT
